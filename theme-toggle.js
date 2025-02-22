92b3df2ff1a78448f4088dabf0b2427d567e46f0
C0RV3R <200282552+C0RV3R@users.noreply.github.com>
2025-02-22 10:56:29 +0300

    Night Mode

---
+++ theme-toggle.js
@@ -0,0 +1,8 @@
+<script>
+    const toggleBtn = document.querySelector('.toggle-btn');
+    const body = document.body;
+
+    toggleBtn.addEventListener('click', () => {
+        body.classList.toggle('dark');
+    });
+</script>
