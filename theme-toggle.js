a9283d051da8ae264dfd46c4d07d5273eb4e2645
C0RV3R <200282552+C0RV3R@users.noreply.github.com>
2025-02-22 11:07:19 +0300

    nightmodev2

--- theme-toggle.js
+++ theme-toggle.js
@@ -3,4 +3,10 @@ const body = document.body;
 
 toggleBtn.addEventListener('click', () => {
     body.classList.toggle('dark');
+
+    if (body.classList.contains('dark')) {
+        toggleBtn.textContent = '🌞';
+    } else {
+        toggleBtn.textContent = '🌙';
+    }
 });
