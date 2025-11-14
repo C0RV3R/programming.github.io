0ebbf7d4b8d0ba02763f3e498c1310785b2584c8
C0RV3R <emre.selvitopu1619@gmail.com>
2025-11-14 13:58:32 +0300

    -

--- assets/js/footer.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("../constants/footer.html")
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById("footer").innerHTML = data;
-    });
-});
